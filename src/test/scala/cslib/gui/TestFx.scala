0514efc5415b880a039fad8817d6f4ba7adae7e4
bjornregnell <bjorn.regnell@cs.lth.se>
2018-04-17 17:55:27 +0200

    add test case for Fx wrapper

---
+++ src/test/scala/cslib/gui/TestFx.scala
@@ -0,0 +1,24 @@
+package cslib.gui
+
+/** run in `sbt:cslib>` with command `Test/runMain cslib.gui.TestFx` */
+object TestFx {
+  def main(args: Array[String]): Unit = {
+    println("You should see a black window with a green line for 1 second.")
+    Fx.newStage { s =>
+        s.show
+        s.setTitle("TestFx")
+        val black = javafx.scene.paint.Color.BLACK
+        val green = javafx.scene.paint.Color.GREEN
+        val root = new javafx.scene.layout.VBox
+        s.setScene(new javafx.scene.Scene(root, 800, 600, black))
+        root.setBackground(javafx.scene.layout.Background.EMPTY)
+        val canvas = new javafx.scene.canvas.Canvas(800, 600)
+        canvas.getGraphicsContext2D.setStroke(green)
+        canvas.getGraphicsContext2D.strokeLine(0,0,100,100)
+        canvas.getGraphicsContext2D.strokeText("Hello Fx!", 105, 105)
+        root.getChildren.add(canvas)
+    }
+    Thread.sleep(1000)
+    Fx.exit()
+  }
+}
