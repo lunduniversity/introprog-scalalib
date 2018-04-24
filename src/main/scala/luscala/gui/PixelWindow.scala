2651f0bbbfd93c484df3e2e97ae237755607c6c5
bjornregnell <bjorn.regnell@cs.lth.se>
2018-04-24 17:45:56 +0200

    update PixelWindow add new funtionality

--- src/main/scala/luscala/gui/PixelWindow.scala
+++ src/main/scala/luscala/gui/PixelWindow.scala
@@ -1,6 +1,7 @@
+package luscala.gui
+
 object PixelWindow {
   def systemExit(): Unit = System.exit(0)
-
   def delay(millis: Long): Unit = Thread.sleep(millis)
 
   object Event {
@@ -15,11 +16,122 @@ object PixelWindow {
   }
 }
 
-class PixelWindow {
-  private lazy val frame = ???
+class PixelWindow(
+  val width: Int = 1000,
+  val height: Int = 1000,
+  val title: String = "PixelWindow",
+  val background: java.awt.Color = java.awt.Color.BLACK
+) {
 
-  private class UnderlyingCanvas(size: (Int, Int)) extends javax.swing.JPanel {
+  import PixelWindow.Event
 
+  var x = 0
+  var y = 0
+  /*private*/ val frame = new javax.swing.JFrame(title)
+  /*private*/ val canvas = new Swing.CanvasPanel(width, height, background)
+
+  private val queueCapacity = 1000
+  private val eventQueue =
+    new java.util.concurrent.LinkedBlockingQueue[java.awt.AWTEvent](queueCapacity)
+
+  private var _lastEventType = Event.Undefined
+  def lastEventType: String = _lastEventType
+
+  protected var _lastKeyText = ""
+  def lastKeyText: String = _lastKeyText
+
+  protected var _lastMousePos = (0, 0)
+  def lastMousePos: (Int, Int) = _lastMousePos
+
+  private def handleEvent(e: java.awt.AWTEvent): Unit = e match {
+    case me: java.awt.event.MouseEvent =>
+      _lastMousePos = (me.getX, me.getY)
+      me.getID match {
+        case java.awt.event.MouseEvent.MOUSE_PRESSED =>
+          _lastEventType = Event.MousePressed
+        case java.awt.event.MouseEvent.MOUSE_RELEASED =>
+            _lastEventType = Event.MouseReleased
+        case _ => println(s"Unknown mouse event: $e")
+      }
+
+    case ke: java.awt.event.KeyEvent =>
+      if (ke.getKeyChar == java.awt.event.KeyEvent.CHAR_UNDEFINED)
+        _lastKeyText = java.awt.event.KeyEvent.getKeyText(ke.getKeyCode)
+      else _lastKeyText = ke.getKeyChar.toString
+
+      println(s"key event: $ke _lastKeyText = ${_lastKeyText}")
+      ke.getID match {
+        case java.awt.event.KeyEvent.KEY_PRESSED =>
+          _lastEventType = Event.KeyPressed
+        case java.awt.event.KeyEvent.KEY_RELEASED =>
+          _lastEventType = Event.KeyReleased
+        case _ => println(s"Unknown key event: $e")
+      }
+
+    case _ => println(s"Unknown event: $e")
   }
 
+  def awaitEvent(timeoutInMillis: Long): Unit = {
+    val e = eventQueue.poll(timeoutInMillis, java.util.concurrent.TimeUnit.MILLISECONDS)
+    if (e != null) handleEvent(e)
+    else _lastEventType = Event.Undefined
+  }
+
+  def moveTo(pos: (Int, Int)): Unit = { x = pos._1; y = pos._2 }
+
+  def lineTo(pos: (Int, Int)): Unit = {
+    canvas.line(x, y, pos._1, pos._2)
+    x = pos._1; y = pos._2
+  }
+
+  def open(): Unit = frame.setVisible(true)
+
+  def close(): Unit = { frame.setVisible(false); frame.dispose() }
+
+  def clear(): Unit = canvas.clear()
+
+  def drawString(s: String) = canvas.drawString(s, x, y)
+
+  def setTextSize(pts: Int) = canvas.textSize = pts
+
+  def setLineColor(c: java.awt.Color): Unit = canvas.lineColor = c
+
+  def setLineWidth(w: Int): Unit = canvas.lineWidth = w
+
+  def setFullScreen(isActivate: Boolean): Unit =
+    if (isActivate) Swing.screen.enterFullScreen(frame)
+    else Swing.screen.exitFullScreen(frame)
+
+  Swing.init() // to setPlatformSpecificLookAndFeel
+  javax.swing.JFrame.setDefaultLookAndFeelDecorated(true)
+
+  frame.addWindowListener(new java.awt.event.WindowAdapter {
+    override def windowClosing(e: java.awt.event.WindowEvent): Unit = {
+      //eventQueue.offer(e);
+      frame.setVisible(false)
+      frame.dispose()
+    }
+  })
+
+  frame.addKeyListener(new java.awt.event.KeyAdapter {
+    override def keyPressed(e: java.awt.event.KeyEvent): Unit = eventQueue.offer(e)
+    override def keyReleased(e: java.awt.event.KeyEvent): Unit = eventQueue.offer(e)
+  })
+
+  canvas.addMouseListener(new java.awt.event.MouseAdapter {
+    override def mousePressed(e: java.awt.event.MouseEvent): Unit = eventQueue.offer(e)
+    override def mouseReleased(e: java.awt.event.MouseEvent): Unit = eventQueue.offer(e)
+  })
+
+
+  val box = new javax.swing.Box(javax.swing.BoxLayout.Y_AXIS)
+  box.setBackground(java.awt.Color.RED)
+  box.add(javax.swing.Box.createVerticalGlue())
+  box.add(canvas)
+  box.add(javax.swing.Box.createVerticalGlue())
+  frame.add(box)
+
+  frame.getContentPane().setBackground(java.awt.Color.BLACK.brighter.brighter)
+  frame.pack()
+  frame.setVisible(true)
 }
--- src/main/scala/luscala/gui/Swing.scala
+++ src/main/scala/luscala/gui/Swing.scala
@@ -1,6 +1,12 @@
 package luscala.gui
 
 object Swing {
+
+  def runInSwingThread(callback: => Unit): Unit =
+    javax.swing.SwingUtilities.invokeLater(() => callback)
+
+  def apply(callback: => Unit): Unit = runInSwingThread(callback)
+
   def installedLookAndFeels: Vector[String] =
     javax.swing.UIManager.getInstalledLookAndFeels.toVector.map(_.getClassName)
 
@@ -9,6 +15,13 @@ object Swing {
 
   def isOS(partOfName: String): Boolean =
     scala.sys.props("os.name").toLowerCase.contains(partOfName.toLowerCase)
+
+  private var isInit = false
+
+  def init(): Unit = if (!isInit) {
+    setPlatformSpecificLookAndFeel()
+    isInit = true
+  }
 
   def setPlatformSpecificLookAndFeel(): Unit = {
     import javax.swing.UIManager.setLookAndFeel
@@ -19,11 +32,112 @@ object Swing {
       javax.swing.UIManager.getSystemLookAndFeelClassName()
     )
   }
+
   def fileDialog(buttonText: String = "Open", startDir: String = "~"): String = {
     val fs = new javax.swing.JFileChooser(new java.io.File(startDir))
     fs.showDialog(null, buttonText) match {
       case 0 => Option(fs.getSelectedFile.toString).getOrElse("")
       case _ => ""
+    }
+  }
+
+  def invertColor(c: java.awt.Color): java.awt.Color =
+    new java.awt.Color(255 - c.getRed, 255 - c.getGreen, 255 - c.getBlue)
+
+  class CanvasPanel(
+    val initWidth: Int,
+    val initHeight: Int,
+    var backgroundColor: java.awt.Color = java.awt.Color.BLACK,
+    var lineColor: java.awt.Color =  java.awt.Color.WHITE,
+    var lineWidth: Int = 1,
+    var textSize: Int = 20
+  ) extends javax.swing.JPanel {
+    val img: java.awt.image.BufferedImage = java.awt.GraphicsEnvironment
+      .getLocalGraphicsEnvironment
+      .getDefaultScreenDevice
+      .getDefaultConfiguration
+      .createCompatibleImage(initWidth, initHeight, java.awt.Transparency.OPAQUE)
+
+    setBackground(backgroundColor)
+		setDoubleBuffered(true)
+		setPreferredSize(new java.awt.Dimension(initWidth, initHeight))
+		setMinimumSize(new java.awt.Dimension(initWidth, initHeight))
+		setMaximumSize(new java.awt.Dimension(initWidth, initHeight))
+		clear()
+
+    override def paintComponent(g: java.awt.Graphics): Unit = g.drawImage(img, 0, 0, this)
+
+  	override def imageUpdate(img: java.awt.Image, infoFlags: Int, x: Int, y: Int, width: Int, height: Int): Boolean = {
+  		repaint()
+      true
+  	}
+
+  	def clear(): Unit = {
+  		val g = img.createGraphics()
+  		g.setColor(backgroundColor)
+  		g.fillRect(0, 0, initWidth, initHeight)
+  		repaint()
+  	}
+
+  	def line(x1: Int, y1: Int, x2: Int, y2: Int): Unit = {
+      import java.awt.BasicStroke
+  		val g = img.createGraphics()
+  		g.setColor(lineColor)
+      val s = new BasicStroke(lineWidth.toFloat, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER)
+  		g.setStroke(s)
+  		g.drawLine(x1, y1, x2, y2)
+  		repaint()
+  	}
+
+  	def drawString(txt: String, x: Int, y: Int): Unit = {
+  		val g = img.createGraphics()
+  		g.setColor(lineColor)
+      g.setRenderingHint(
+        java.awt.RenderingHints.KEY_TEXT_ANTIALIASING,
+        java.awt.RenderingHints.VALUE_TEXT_ANTIALIAS_ON)
+      val f = g.getFont
+      g.setFont(new java.awt.Font(f.getName,f.getStyle,textSize))
+  		g.drawString(txt, x, y)
+  		repaint()
+  	}
+  }
+
+  object screen {
+    def device = java.awt.GraphicsEnvironment.getLocalGraphicsEnvironment.getDefaultScreenDevice
+
+    def isSupported = device.isFullScreenSupported
+
+    def isFullScreen = device.getFullScreenWindow != null
+
+    def setUndecorated(w: java.awt.Window, state: Boolean): Unit =  w match {
+      case f: javax.swing.JFrame =>
+        f.dispose()
+        f.setUndecorated(state)
+        f.pack()
+        f.setVisible(true)
+      case _ =>
+    }
+
+    def exitFullScreen(w: java.awt.Window): Unit =
+      if (isSupported) {
+        device.setFullScreenWindow(null)
+        setUndecorated(w, false)
+      }
+
+   def enterFullScreen(w: java.awt.Window): Unit =
+     if (isSupported) {
+       setUndecorated(w, true)
+       device.setFullScreenWindow(w)
+     }
+
+    def toggleFullScreen(w: java.awt.Window): Unit =
+      if (isFullScreen) exitFullScreen(w) else enterFullScreen(w)
+
+    def toggleDecorations(w: java.awt.Window): Unit = w match {
+      case f: javax.swing.JFrame =>
+        if (f.isUndecorated) setUndecorated(f, false)
+        else setUndecorated(f, true)
+      case _ =>
     }
   }
 }
