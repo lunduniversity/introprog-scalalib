4409b9c0dcdc4f7f9bc9579be373ec5cb9c2b3db
bjornregnell <bjorn.regnell@cs.lth.se>
2018-05-25 19:08:52 +0200

    add Dialog object for user input

--- src/main/scala/introprog/Dialog.scala
+++ src/main/scala/introprog/Dialog.scala
@@ -1,13 +1,37 @@
 package introprog
 
 object Dialog {
+  import javax.swing.{JFileChooser, JOptionPane}
   Swing.init()  // set platform-specific look-and-feel
 
-  def chooseFile(buttonText: String = "Open", startDir: String = "~"): String = {
-    val fs = new javax.swing.JFileChooser(new java.io.File(startDir))
-    fs.showDialog(null, buttonText) match {
-      case 0 => Option(fs.getSelectedFile.toString).getOrElse("")
+  /** Show a file choice dialog starting in `startDir` with confirm `button` text. */
+  def file(button: String = "Open", startDir: String = "~"): String = {
+    val fs = new JFileChooser(new java.io.File(startDir))
+    fs.showDialog(null, button) match {
+      case JFileChooser.APPROVE_OPTION => Option(fs.getSelectedFile.toString).getOrElse("")
       case _ => ""
     }
   }
+
+  /** Show a dialog with a `message` text. */
+  def show(message: String): Unit = JOptionPane.showMessageDialog(null, message)
+
+  /** Show a `message` askin for input with `init` value. Return user input.*/
+  def input(message: String, init: String = ""): String =
+    JOptionPane.showInputDialog(message, init)
+
+  /** Show a confirmation dialog with `question` and OK and Cancel buttons. */
+  def isOK(question: String = "Are you sure?", title: String = "Confirm"): Boolean =
+    JOptionPane.showConfirmDialog(
+      null, question, title, JOptionPane.OK_CANCEL_OPTION) ==
+        JOptionPane.OK_OPTION
+
+  /** Show a selection dialog with `buttons`. Return a string with the chosen button text. */
+  def select(message: String, buttons: Seq[String], title: String = "Select"): String =
+    scala.util.Try{
+      val chosenIndex =
+        JOptionPane.showOptionDialog(null, message, title, JOptionPane.DEFAULT_OPTION,
+          JOptionPane.QUESTION_MESSAGE, null, buttons.reverse.toArray, null)
+      buttons(buttons.length - 1 - chosenIndex)
+    }.getOrElse("")
 }
