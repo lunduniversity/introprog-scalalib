f49acedde199c4fab39634e330104864b7c8515c
bjornregnell <bjorn.regnell@cs.lth.se>
2018-05-25 13:26:58 +0200

    test fullscreen

--- src/test/scala/TestPixelWindow.scala
+++ src/test/scala/TestPixelWindow.scala
@@ -1,6 +1,6 @@
 object TestPixelWindow {
   import introprog._
-  
+
   val w = new PixelWindow(400, 300, "Hello PixelWindow!")
 
   def square(topLeft: (Int, Int))(side: Int): Unit = {
@@ -12,13 +12,14 @@ object TestPixelWindow {
   }
 
   def main(args: Array[String]): Unit = {
-    println("Drawing a red square in a PixelWindow. Close window to exit.")
+    println("Drawing a red square in a PixelWindow.")
+    println("Press F11 for full screen. Close window to exit.")
     w.lineWidth = 2
     w.color = java.awt.Color.red
     square(200, 100)(50)
     while (w.lastEventType != PixelWindow.Event.WindowClosed) {
       w.awaitEvent(10)  // wait for next event for max 10 milliseconds
-      println(s"lastEventType == " + w.lastEventType)
+      //println(s"lastEventType == " + w.lastEventType)
       w.lastEventType match {
         case PixelWindow.Event.KeyPressed    => println("lastKey == " + w.lastKey)
         case PixelWindow.Event.KeyReleased   => println("lastKey == " + w.lastKey)
@@ -27,7 +28,9 @@ object TestPixelWindow {
         case PixelWindow.Event.WindowClosed  => println("Goodbye!")
         case _ =>
       }
-      PixelWindow.delay(1000) // wait for 1 second
+      if (w.lastEventType == PixelWindow.Event.KeyPressed && w.lastKey == "F11")
+        Swing.Screen.toggleFullScreen(w.frame)
+      PixelWindow.delay(10) // wait for 1 second
     }
   }
 }
